b87719115aca6668e070f9aa2c00e139856dfa9c
Matt Zamora <mzamora2899@gmail.com>
2022-04-11 20:16:54 -0600

    finish initial fe setup for multi-sig-wallet

---
+++ multi-sig/client/src/pages/Home.js
@@ -0,0 +1,13 @@
+
+const Home = () => {
+
+  return (
+    <div className="h-screen w-full">
+      <div className="flex justify-center w-full">
+      <p className="text-5xl text-center">Multi-Signature Wallet</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
