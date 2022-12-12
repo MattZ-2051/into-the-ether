238368a7e5b40ce21f87d97e963ee691670206a5
Matt Zamora <mzamora2899@gmail.com>
2022-12-12 14:46:25 -0700

    add notes

--- solidity-notes/upgradable-proxy/notes.sol
+++ solidity-notes/upgradable-proxy/notes.sol
@@ -4,29 +4,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract CounterV1 {
-  uint public count;
+    uint256 public count;
 
-  function inc() external {
-    count += 1;
-  }
+    function inc() external {
+        count += 1;
+    }
 }
 
 contract CounterV2 {
-  uint public count;
+    uint256 public count;
 
-  function inc() external {
-    count += 1;
-  }
+    function inc() external {
+        count += 1;
+    }
 
-  function dev() external {
-    count -= 1;
-  }
+    function dev() external {
+        count -= 1;
+    }
 }
-
 
 // this will be an example of how not to do a proxy contract
 contract BuggyProxy {
+    address public implementation;
+    address public admin;
 
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function _delegate() private {
+        (bool ok, bytes memory res) = implementation.delegatecall(msg.data);
+        require(ok, "delegatecall failed");
+    }
+
+    fallback() external payable {
+        _delegate();
+    }
+
+    receive() external payable {
+        _delegate();
+    }
+
+    function upgradeTo(address _implementation) external {
+        require(msg.sender == admin, "Not Authorized");
+        implementation = _implementation;
+    }
 }
