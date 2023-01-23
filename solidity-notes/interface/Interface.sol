079fc80b6c94f81b1b0ad29339837dd85f4e569f
Matt Zamora <mzamora2899@gmail.com>
2023-01-22 18:48:45 -0700

    format

--- solidity-notes/interface/Interface.sol
+++ solidity-notes/interface/Interface.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.8.3;
 
+// Interfaces are used to call contracts where you dont have the code
+
+interface ICounter {
+    function count() external view returns (uint256);
+
+    function inc() external;
+}
+
 contract Counter {
     uint256 public count;
 
@@ -13,7 +21,10 @@ contract Counter {
 }
 
 contract CallInterface {
+    uint256 public count;
+
     function examples(address _counter) external {
-        Counter(_counter).inc();
+        ICounter(_counter).inc();
+        count = ICounter(_counter).count();
     }
 }
