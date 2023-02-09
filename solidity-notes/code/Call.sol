47f7a37f33306437f2557c557ce86bd5ad6d04e1
Matt Zamora <mzamora2899@gmail.com>
2023-02-09 12:14:42 -0700

    ethernaut notes

--- solidity-notes/code/Call.sol
+++ solidity-notes/code/Call.sol
@@ -1,2 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
+
+contract TestCall {
+    string public message;
+    uint256 public x;
+
+    event Log(string message);
+
+    fallback() external payable {
+        emit Log("fallback was called");
+    }
+
+    function foo(string memory _message, uint256 _x)
+        external
+        payable
+        returns (bool, uint256)
+    {
+        message = _message;
+        x = _x;
+        return (true, 999);
+    }
+}
+
+contract Call {
+    function callFoo(address _test) external {
+        _test.call(abi.encodeWithSignature("foo(string.uint256)"));
+    }
+}
