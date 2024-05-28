20bee3beb7588d4dc694a7b214799731fd946037
Matt Zamora <mzamora2899@gmail.com>
2024-05-27 19:26:18 -0600

    notes

--- sol-notes-2/src/EvmStorage/StateVars.sol
+++ sol-notes-2/src/EvmStorage/StateVars.sol
@@ -17,3 +17,66 @@ contract YuulIntro {
         return s;
     }
 }
+
+contract EVMStorageSingleSlot {
+    // EVM storage
+    // 2**256 slots, each slot can store up to 32 bytes
+    // Slots are assigned in the order the state variables are declared
+    // Data < 32 bytes are packed into a slot (right to left)
+
+    // Single variable stored in one slot
+    // slot 0
+    uint256 public s_x;
+    // slot 1
+    uint256 public s_y;
+    // slot 2
+    bytes32 public s_z;
+
+    function test_sstore() public {
+        // sstore(k, v) = store v to slot k
+        assembly {
+            // this is the same as writing s_x = 1234; in sol
+            sstore(0, 1234)
+            sstore(1, 456)
+            sstore(2, 0xababab)
+        }
+    }
+
+    function test_sstore_again() public {
+        // we can access the slot of the state var by using (var_name).slot
+        assembly {
+            sstore(s_x.slot, 456)
+            sstore(s_y.slot, 123)
+            sstore(s_z.slot, 0xbbbbb)
+        }
+    }
+
+    function test_sload()
+        public
+        view
+        returns (uint256 x, uint256 y, bytes32 z)
+    {
+        // sload(k) = load 32 bytes from slot k
+        assembly {
+            x := sload(0)
+            y := sload(1)
+            z := sload(2)
+        }
+
+        return (x, y, z);
+    }
+
+    function test_sload_again()
+        public
+        view
+        returns (uint256 x, uint256 y, bytes32 z)
+    {
+        assembly {
+            x := sload(s_x.slot)
+            y := sload(s_y.slot)
+            z := sload(s_z.slot)
+        }
+
+        return (x, y, z);
+    }
+}
